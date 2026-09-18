c07fca4b61ded914b506b9554a7a22988eead7ec
N4ncyhdzz <nancy.judithhm23@gmail.com>
2026-09-18 11:30:45 -0600

    Clase de ayer dataGrid

--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,14 +13,39 @@ namespace MyApp
 {
     public partial class Form1 : Form
     {
+        List<Persona> personas = new List<Persona>();
+
         public Form1()
         {
             InitializeComponent();
+          
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // 1. Primero agregamos los datos a la lista
+            personas.Add(new Persona(1, "Nancy Judith", "8711181633"));
+            personas.Add(new Persona(2, "Yamilet Esmeralda", "8711181634"));
+            personas.Add(new Persona(3, "Jorge Luis", "8711181635"));
+            personas.Add(new Persona(4, "Jorge Luis", "8711181636"));
+
+            // 2. Después recorremos la lista para pintarlos en la tabla
+            foreach (var Persona in personas)
+            {
+                dgvInformacion.Rows.Add();
+                dgvInformacion[0, dgvInformacion.Rows.Count - 1].Value = Persona.Id;
+                dgvInformacion[1, dgvInformacion.Rows.Count - 1].Value = Persona.nombre;
+                dgvInformacion[2, dgvInformacion.Rows.Count - 1].Value = Persona.telefono;
+            }
         }
 
         private void btnAgregar_Click(object sender, EventArgs e)
         {
-
+            dgvInformacion.Rows.Add();
+            dgvInformacion[0, dgvInformacion.Rows.Count - 1].Value = dgvInformacion.Rows.Count;
+            dgvInformacion[1, dgvInformacion.Rows.Count - 1].Value = txtNombre.Text;
+            dgvInformacion[2, dgvInformacion.Rows.Count - 1].Value = mtbTelefono.Text;
         }
     }
 }
---
+++ Persona.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp
+{
+    internal class Persona
+    {
+
+        public int Id { get; set; }
+        public string nombre { get; set; }
+        public string telefono { get; set; }
+
+        public Persona (int id, string nombre, string telefono)
+        {
+         this.Id = id;
+         this.nombre = nombre;
+         this.telefono = telefono;
+        }
+    }
+}
